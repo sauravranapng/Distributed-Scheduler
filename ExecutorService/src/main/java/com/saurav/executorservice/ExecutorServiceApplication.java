a4759405acc4a8f14673f2f1a2104fa94f8c10d6
sauravranapng <sauravranabhari2002@gmail.com>
2026-07-03 22:55:13 +0530

    adding ExecutorService starter project

---
+++ ExecutorService/src/main/java/com/saurav/executorservice/ExecutorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saurav.executorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutorServiceApplication.class, args);
+	}
+
+}
---
+++ ExecutorService/src/test/java/com/saurav/executorservice/ExecutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.executorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
